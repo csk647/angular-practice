fadf0ec8fd6c172733d5083ef432d71889c8d2f6
csk647@gmail.com <csk647@gmail.com>
2021-10-05 00:59:15 +0900

    input output으로 데이터 이동 테스트

--- src/app/my-assign/my-assign.component.ts
+++ src/app/my-assign/my-assign.component.ts
@@ -6,8 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-assign.component.css']
 })
 export class MyAssignComponent implements OnInit {
+  name="init-name";
+  formName=""
 
   constructor() { }
+
+  onReceiveFunc(evtName : string){
+    console.log(evtName)
+    this.name = evtName;
+  }
+
+  onReceiveForm(formVal:any){
+    console.log(formVal)
+    this.formName = formVal.value;
+
+  }
 
   ngOnInit(): void {
   }
--- src/app/name-editor/name-editor.component.ts
+++ src/app/name-editor/name-editor.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { FormControl } from '@angular/forms'; //1. formControl을 import 한다.
+// import { EventEmitter } from 'stream';
 
 @Component({
   selector: 'app-name-editor',
@@ -9,11 +10,17 @@ import { FormControl } from '@angular/forms'; //1. formControl을 import 한다.
 export class NameEditorComponent {
   assignName = new FormControl(''); // new FormControl를 해준다.
 
+  @Output() outputName = new EventEmitter<string>();
+  @Output() outAssignName = new EventEmitter<FormControl>();
+  myName="aeri-emit"
+
   constructor() {
    }
 
   updateName(e:Event) {
-    this.assignName.setValue('aeri') 
+    // this.assignName.setValue('aeri') ;
+    this.outputName.emit(this.myName);
+    this.outAssignName.emit(this.assignName)
   }
 
   ngOnInit(): void {
